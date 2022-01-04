b78c5f487e2c91bf8b15b634550f1642d6dc0e99
taem98 <taemi@chungbuk.ac.kr>
2022-01-04 17:28:25 +0900

    add remove small width code / add cound code

---
+++ should_change/remove_small_width/make_removed_txt.py
@@ -0,0 +1,21 @@
+import os
+import pickle
+import shutil
+import cv2
+
+img_dir_path = '../../images/split_data/direction/test_removed/left/'
+img_list = os.listdir(img_dir_path)
+img_list.sort()
+
+remove_list = []
+
+for img_name in img_list:
+    img = cv2.imread(img_dir_path + img_name)
+    h, w, _ = img.shape
+    if (h/w) > 5 :
+        remove_list.append(img_name)
+
+with open("./removed_test_left.txt", 'w') as f:
+    for img_name in remove_list:
+        f.write(img_name+'\n')
+
