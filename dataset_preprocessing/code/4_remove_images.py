fef2c29e369e14cfdffcf760b4a7ba7fec023e68
jihyeon-yu <fbwlgus6@naver.com>
2022-01-18 17:53:36 +0900

    remove images in remove_list

--- dataset_preprocessing/code/4_remove_images.py
+++ dataset_preprocessing/code/4_remove_images.py
@@ -1,30 +1,45 @@
-
-
 import os
+import glob
+from posixpath import split
+import re
 
-# with open('./removed_test_left.txt', 'r') as f:
-#         texts = f.readlines()
 
-with open("./removed_test/*.txt", 'r') as f:
-    texts = f.readlines()
 
-# dir_path = "../images/"
 
-dir_path = "../images/**/"
+# get the directory list of videos
 
-for name in texts:
-    if os.path.isfile(dir_path+name[:-1]):
-        os.remove(dir_path+name[:-1])
-        # pass
-    else:
-        print("no file : ", dir_path+name)
+dir_path = "../images/"
+list_dir = os.listdir(dir_path)
+list_dir.sort()
 
-# for name in os.listdir(dir_path):
-#     with open(os.path.join(dir_path, name), 'r') as f:
-#         if os.path.isfile(dir_path+name[:-1]):
-#            os.remove(dir_path+name[:-1])
-#            # pass
-#         else:
-#            print("no file : ", dir_path+name)
 
-        
+
+# removed_video_name_list
+
+img_path = "../removed_test/"
+
+img_dir = []
+txt_list = os.listdir(img_path)
+txt_list.sort()
+for i in txt_list:
+    split_list = re.split('[_.]',i)
+    split_name = split_list[2]+"_"+split_list[3]
+    img_dir.append(split_name)
+
+
+
+# remove the images in removed_test list
+
+
+for imgs in img_dir:
+    txt_path = "../removed_test/removed_test_{}.txt".format(imgs)
+    file = open(txt_path, 'r')
+    read_file = file.readlines()
+    for img_name in read_file:
+        impath = dir_path+txt_path[29:39]+"/"+img_name
+        imlist = impath.split('\n')
+        for name in imlist:
+            if os.path.isfile(name):
+                os.remove(name)
+    
+
